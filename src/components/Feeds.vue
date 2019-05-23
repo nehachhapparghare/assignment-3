b6e6d4ecf6170bc0092c0c60f56b81495b236a63
Neha Chhapparghare <Neha.Chhapparghare@rsicorp.local>
2019-05-23 14:33:35 +0530

    final 5/23

--- src/components/Feeds.vue
+++ src/components/Feeds.vue
@@ -3,7 +3,7 @@
     <v-container fluid fill-height>
       <v-layout align-center justify-center row fill-height>
         <v-flex xs12 md8 mx-2>
-          <v-card class="elevation-3">
+          <v-card class="elevation-3" >
             <v-card-title>
               <v-spacer></v-spacer>
               <v-text-field
@@ -42,30 +42,23 @@
               <template v-slot:items="props">
                 <tr :active="props.selected">
                   <td>
-                    <v-checkbox
-                      :input-value="props.selected"
-                      primary
-                      hide-details
-                      v-on:click="getFeedDetails(props.item)"
-                    ></v-checkbox>
+                    <v-checkbox :input-value="props.selected" primary hide-details></v-checkbox>
                   </td>
                   <td class="text-center subheading">{{ props.item.id }}</td>
                   <td class="text-center subheading">{{ props.item.type }}</td>
                   <td class="text-center subheading">{{ props.item.title }}</td>
                   <td class="text-center subheading">
-                    <v-text-field
-                      v-model="props.item.comment"
-                      v-on:keyup.enter="getComment(props.item.comment)"
-                    ></v-text-field>
+                    <v-text-field v-model="props.item.comment" v-on:change="showFeed(props.item)"></v-text-field>
                   </td>
                   <td>
-                    <v-flex xs12 mt-4 v-on:click="getSelectedItem(props.item.item)">
+                    <v-flex xs12 mt-4>
                       <v-select
                         :items="getItem"
                         label="Select"
                         solo
                         flat
                         class="subheading"
+                        v-on:change="showFeed(props.item)"
                         v-model="props.item.item"
                       ></v-select>
                     </v-flex>
@@ -86,7 +79,6 @@
     data: () => ({
       selected: [],
       list: [],
-      item: [],
       search: "",
       comment: "",
       select: "",
@@ -116,21 +108,8 @@
         if (this.selected.length) this.selected = [];
         else this.selected = this.getList.slice();
       },
-      showFeedDetails(field, select) {
-        console.log(select);
-      },
-      getComment(comment) {
-        this.comment = comment;
-        console.log(this.comment);
-      },
-      getFeedDetails(feed) {
-        if (this.selectedItem) {
-          feed[this.selectedItem] = this.selected;
-        }
-        console.log(feed);
-      },
-      getSelectedItem(item) {
-        this.selectedItem = item;
+      showFeed(item) {
+        console.log(item);
       }
     }
   };
